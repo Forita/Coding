bd4323ae0e768fe02af023d4f09fa45c44234d63
Forita <Foritarui@gmail.com>
2018-09-01 23:00:32 -0400

    Update two-sum-iii-data-structure-design.py

--- Python/two-sum-iii-data-structure-design.py
+++ Python/two-sum-iii-data-structure-design.py
@@ -1,12 +1,9 @@
 from __future__ import print_function
-# Time:  O(n)
-# Space: O(n)
-
+# O(n) runtime, O(n) space.
 # Design and implement a TwoSum class. It should support the following operations: add and find.
-#
 # add - Add the number to an internal data structure.
 # find - Find if there exists any pair of numbers which sum is equal to the value.
-#
+
 # For example,
 # add(1); add(3); add(5);
 # find(4) -> true
