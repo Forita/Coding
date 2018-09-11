887a29bafdf2fe4eb272d8e3bd2b19dc0659e3e8
Forita <Foritarui@gmail.com>
2018-09-11 16:16:47 -0400

    Create one-edit-distance.py

---
+++ Python/one-edit-distance.py
@@ -0,0 +1,34 @@
+from __future__ import print_function
+# O(m + n) runtime, O(1) space.
+#
+# Given two strings S and T, determine if they are both one edit distance apart.
+#
+
+class Solution(object):
+    def isOneEditDistance(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        m, n = len(s), len(t)
+        if m > n:
+            return self.isOneEditDistance(t, s)
+        if n - m > 1:
+            return False
+
+        i, shift = 0, n - m
+        while i < m and s[i] == t[i]:
+            i += 1
+        if i == m:
+            return shift > 0
+        if shift == 0:
+            i += 1
+        while i < m and s[i] == t[i + shift]:
+            i += 1
+
+        return i == m
+
+
+if __name__ == "__main__":
+    print(Solution().isOneEditDistance("teacher", "acher"))
